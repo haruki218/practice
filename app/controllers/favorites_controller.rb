044bb693be132c7f27cab67d03f8fdd7c000deb2
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-28 04:45:12 +0000

    [Update]いいねの非同期通信化実装途中

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,35 @@
 class FavoritesController < ApplicationController
+  before_action :set_book
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    favorite = current_user.favorites.new(book_id: @book.id)
+    if favorite.save
+      respond_to do |format|
+        format.js
+        format.html { redirect_to request.referer }
+      end
+    else
+      logger.error favorite.errors.full_messages
+      head :unprocessable_entity
+    end
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_to request.referer
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    if favorite.destroy
+      respond_to do |format|
+        format.js
+        format.html { redirect_to request.referer }
+      end
+    else
+      logger.error favorite.errors.full_messages
+      head :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 end
