9f77ff396f8a9a71cbf47e8d1c9c710a8bb3c9cc
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-07-28 06:14:16 +0000

    [Update]いいね機能の非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,35 +1,19 @@
 class FavoritesController < ApplicationController
-  before_action :set_book
-
   def create
+    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
-    if favorite.save
-      respond_to do |format|
-        format.js
-        format.html { redirect_to request.referer }
-      end
-    else
-      logger.error favorite.errors.full_messages
-      head :unprocessable_entity
+    favorite.save
+    respond_to do |format|
+      format.js
     end
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    if favorite.destroy
-      respond_to do |format|
-        format.js
-        format.html { redirect_to request.referer }
-      end
-    else
-      logger.error favorite.errors.full_messages
-      head :unprocessable_entity
-    end
-  end
-
-  private
-
-  def set_book
     @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite.destroy
+    respond_to do |format|
+      format.js
+    end
   end
 end
