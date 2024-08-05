41b5914efdf2dfdb3580b109cb6c051bf2b9158a
EC2 Default User <ec2-user@ip-172-31-45-163.ap-northeast-1.compute.internal>
2024-08-05 08:11:21 +0000

    [Add]タグ検索機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
   get "/search", to: "searches#search"
+  get 'tagsearches/search', to: 'tagsearches#search', as: 'tagsearch'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
